5845fa965b25bc27f68c75b852de67df2a91a4d1
Thiago Teixeira Nogueira <tnogueira@uolinc.com>
2017-05-29 18:46:35 -0300

    refactor - chapter 15

--- app/src/main/java/com/example/tnogueira/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/tnogueira/weatherapp/ui/activities/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.tnogueira.weatherapp.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.example.tnogueira.weatherapp.R
 import com.example.tnogueira.weatherapp.domain.commands.RequestForecastCommand
-import com.example.tnogueira.weatherapp.domain.model.Forecast
 import com.example.tnogueira.weatherapp.ui.adapters.ForecastListAdapter
-import org.jetbrains.anko.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,14 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val forecatList: RecyclerView = find(R.id.forecast_list)
-        forecatList.layoutManager = LinearLayoutManager(this)
+        forecastList.layoutManager = LinearLayoutManager(this)
 
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
                 val adapter = ForecastListAdapter(result) { toast(it.date) }
-                forecatList.adapter = adapter
+                forecastList.adapter = adapter
             }
         }
     }
--- app/src/main/java/com/example/tnogueira/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/example/tnogueira/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.tnogueira.weatherapp.R
 import com.example.tnogueira.weatherapp.domain.model.Forecast
 import com.example.tnogueira.weatherapp.domain.model.ForecastList
 import com.example.tnogueira.weatherapp.ui.utils.ctx
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 
 /**
@@ -33,19 +31,14 @@ class ForecastListAdapter(val weekForecast: ForecastList,
     override fun getItemCount(): Int = weekForecast.size
 
     class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
-        private val iconView = view.find<ImageView>(R.id.icon)
-        private val dateView = view.find<TextView>(R.id.date)
-        private val descriptionView = view.find<TextView>(R.id.description)
-        private val maxTemparatureView = view.find<TextView>(R.id.maxTemperature)
-        private val minTemparatureView = view.find<TextView>(R.id.minTemperature)
 
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemparatureView.text = "${high}"
-                minTemparatureView.text = "${low}"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high}"
+                itemView.minTemperature.text = "${low}"
                 itemView.setOnClickListener{ itemClick(this) }
             }
         }
